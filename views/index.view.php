016da5c2cc87e5bbecca53abd05f100797766d85
abbeyGirvan <agirvan@northislandcollege.ca>
2023-10-17 14:52:01 -0700

    Set up and style dummy list of tasks

--- views/index.view.php
+++ views/index.view.php
@@ -16,12 +16,40 @@
             <h2>All Tasks</h2>
         </div>
         <div class="big-content">
-            <h3>room</h3>
-            <ul>
-                <li>kitchen</li>
-                <li>bathroom</li>
-                <li>living room</li>
-            </ul>
+            <div class="rooms">
+                <h3>room</h3>
+                <ul>
+                    <li>kitchen</li>
+                    <li>kitchen</li>
+                    <li>bathroom</li>
+                    <li>living room</li>
+                </ul>
+            </div>
+            <div class="tasks">
+                <h3>task</h3>
+                <ul>
+                    <li>empty dishwasher</li>
+                    <li>load dishwasher</li>
+                    <li>clean shower</li>
+                    <li>sweep floors</li>
+                </ul>
+            </div>
+            <div class="assign">
+                <h3>assigned to</h3>
+                <ul>
+                    <li>Neil</li>
+                    <li>Neil</li>
+                    <li>Andrew</li>
+                    <li>Renee</li>
+                </ul>
+            </div>
+            <div class="status">
+                <h3>status</h3>
+                <input type="checkbox">
+                <input type="checkbox">
+                <input type="checkbox">
+                <input type="checkbox">
+            </div>
         </div>
     </main>
 
