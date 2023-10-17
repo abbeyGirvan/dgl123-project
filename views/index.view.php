5767aabcf6c147ccd466f1e12926ed9de5cddd79
abbeyGirvan <agirvan@northislandcollege.ca>
2023-10-17 13:48:14 -0700

    Set up and link some basic styles

--- views/index.view.php
+++ views/index.view.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Home</title>
-    <link rel="stylesheet" href="../styles.css" />
+    <link rel="stylesheet" href="styles.css" />
 
 </head>
 <body>
@@ -13,15 +13,7 @@
         <nav>
         <div class="content-wrap">
             <div class="container">
-              <a href="index.php">
-                <img
-                  class="logo"
-                  src="images/logo.png"
-                  alt="logo icon"
-                />
-              </a>
-
-              <a href="homelist.php">Home List</a>
+              <a href="index.php">Home List</a>
               <a href="yourtasks.php">Your Tasks</a>
               <a href="addnew.php">Add a Task</a>
               <a href="account.php">Account</a>
