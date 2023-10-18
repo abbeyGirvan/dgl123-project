12d13ea546664df050de669b3e921f00acb089e2
abbeyGirvan <agirvan@northislandcollege.ca>
2023-10-17 22:12:40 -0700

    Attempt to format 'your info' on account page
    still looking a bit wonky, but the basic idea is there

--- views/account.view.php
+++ views/account.view.php
@@ -13,14 +13,17 @@
     <main>
         <?php require 'partials/header.php'; ?>
         <div class="content-wrap side-by-side">
-            <div class="current-user">
-                <h2>YOU</h2>
-                <span class="placeholder"></span>
-                <h3>Neil</h3>
-                <p>'run-away'</p>
-                <span class="placeholder colour-block"></span>
-            </div>
             <div>
+                <h2>YOU</h2>
+                <div class="current-user">
+                    <span class="placeholder"></span>
+                    <h3>Neil</h3>
+                    <h4>nickname:</h4>
+                    <p>'run-away'</p>
+                    <span class="placeholder colour-block"></span>
+                </div>
+            </div>
+            <div class="account-info">
                 <h2>ACCOUNT INFO</h2>
                 <h3>email:</h3>
                 <p>njosten@palmetofoxes.com</p>
