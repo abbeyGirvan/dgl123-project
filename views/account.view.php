e7afaddf2b28a179132e1e728d186fdbffda62a1
abbeyGirvan <agirvan@northislandcollege.ca>
2023-10-17 22:02:51 -0700

    Set up basic structure for account page

--- views/account.view.php
+++ views/account.view.php
@@ -12,7 +12,24 @@
 <body>
     <main>
         <?php require 'partials/header.php'; ?>
-        
+        <div class="content-wrap side-by-side">
+            <div class="current-user">
+                <h2>YOU</h2>
+                <span class="placeholder"></span>
+                <h3>Neil</h3>
+                <p>'run-away'</p>
+                <span class="placeholder colour-block"></span>
+            </div>
+            <div>
+                <h2>ACCOUNT INFO</h2>
+                <h3>email:</h3>
+                <p>njosten@palmetofoxes.com</p>
+                <h3>password</h3>
+                <p>*********</p>
+            </div>
+
+
+        </div>
     </main>
 
 </body>
