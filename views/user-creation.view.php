fdd56aee2cb1b4598eed1eff380563fcd57d031a
abbeyGirvan <agirvan@northislandcollege.ca>
2023-10-25 12:00:18 -0700

    Create some dummy structure to add user profiles
    going to have part of the sign up experience include creating user profiles, likely look like another form input with continue or complete buttons

---
+++ views/user-creation.view.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <title>sign up - more info</title>
+    <link rel="stylesheet" href="styles.css" />
+
+</head>
+
+<body>
+    <main>
+        <?php require 'partials/login-header.php'; ?>
+<!-- 
+    add some code to display account holder name and email here
+    now going to add a form to create user profiles for as many people as the user wants
+    REQUIRED to create one user profile for the account holder, else is extra
+    basically goint to add data/link to table in user table describing profiles on the account.
+-->
+        <div class="content-wrap">
+            <form action="create-profiles" class="create-profiles">
+                <!-- <label for="first-name">first name</label>
+                <input type="text" name="first-name">
+                <label for="last-name">last name</label>
+                <input type="text" name="last-name">
+                <label for="email">email address</label>
+                <input type="text" name="email">
+                <label for="password">password</label>
+                <input type="text" name="password">
+                <label for="confirm-password">confirm password</label>
+                <input type="text" name="confirm-password">
+                <input type="submit" class="log-submit" value="SIGN UP"> -->
+            </form>
+        </div>
+        <div class="content-wrap">
+            <div class="lower-container">
+                <p>already have an account?</p>
+                <button><a href="login.php">log in here</a></button>
+            </div>
+        </div>
+    </main>
+
+</body>
+
+</html>
