c02d8d59d28a5aed41f66aca09b2cd499f3ad2d6
abbeyGirvan <agirvan@northislandcollege.ca>
2023-10-25 11:17:58 -0700

    Wrap login content in div for styling

--- views/login.view.php
+++ views/login.view.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=, initial-scale=1.0">
@@ -7,20 +8,24 @@
     <link rel="stylesheet" href="styles.css" />
 
 </head>
+
 <body>
     <main>
-    <?php require 'partials/login-header.php'; ?>
+        <?php require 'partials/login-header.php'; ?>
 
-    <form action="login">
-        <label for="email">email address</label>
-        <input type="text" name="email">
-        <label for="password">password</label>
-        <input type="text" name="password">
-        <input type="submit">
-    </form>
-    <p>new to squeaky clean?<br>sign up for an account</p>
-    <button>sign up here</button>
+        <div class="content-wrap">
+            <form action="login">
+                <label for="email">email address</label>
+                <input type="text" name="email">
+                <label for="password">password</label>
+                <input type="text" name="password">
+                <input type="submit">
+            </form>
+            <p>new to squeaky clean?<br>sign up for an account</p>
+            <button>sign up here</button>
+        </div>
     </main>
-</body>
-</html>
 
+</body>
+
+</html>
