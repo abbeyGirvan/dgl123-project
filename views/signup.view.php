cb9121b3853aff4944ec90d58e09e88974be8521
abbeyGirvan <agirvan@northislandcollege.ca>
2023-10-25 11:37:18 -0700

    Add base html structure to signup page

---
+++ views/signup.view.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <title>sign up</title>
+    <link rel="stylesheet" href="styles.css" />
+
+</head>
+
+<body>
+    <main>
+        <?php require 'partials/login-header.php'; ?>
+    </main>
+
+</body>
+
+</html>
