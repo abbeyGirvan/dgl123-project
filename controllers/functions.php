47dc3d00421cc5278ce53111948f0554e6a841de
abbeyGirvan <agirvan@northislandcollege.ca>
2023-11-01 09:13:42 -0700

    Add functions.php -- contains psuedo code atm

---
+++ controllers/functions.php
@@ -0,0 +1,7 @@
+<?php
+
+function signedIn(/*cookie value of signed in firstname? */) {
+    //if has value !null in cookie for firstname, continue on  
+    //else redirect page to log-in page
+}
+
