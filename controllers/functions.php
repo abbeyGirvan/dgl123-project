20a0b383ecabb52cd793f187a8478402dfadaa67
abbeyGirvan <agirvan@northislandcollege.ca>
2023-11-01 10:50:22 -0700

    Bring in dd() from laracast videos

--- controllers/functions.php
+++ controllers/functions.php
@@ -5,3 +5,12 @@ function signedIn(/*cookie value of signed in firstname? */) {
     //else redirect page to log-in page
 }
 
+
+//from Laracast videos 'die and dump'
+function dd($value){
+    echo "<pre>";
+    var_dump($value);
+    echo "</pre>";
+
+    die();
+}
