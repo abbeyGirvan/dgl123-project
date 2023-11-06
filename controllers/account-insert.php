1b80b12bcfb8db4b59125e90a83bb31800e59f24
abbeyGirvan <agirvan@northislandcollege.ca>
2023-11-05 21:05:21 -0800

    Copy task-insert and refactor to add account to accounts table

---
+++ controllers/account-insert.php
@@ -0,0 +1,35 @@
+<?php
+
+require 'db-connection.php';
+
+// add single quote to start and end of data
+$firstName = "'".$_POST["first-name"]."'";
+$lastName = "'".$_POST["last-name"]."'";
+$email = "'".$_POST["email"]."'";
+$password = "'".$_POST["password"]."'";
+$confirmPassword = "'".$_POST["confirm-password"]."'";
+
+//echo '<script> alert("hello *'.$dueDate.'")</script>';
+
+
+
+// Insert record
+$sql = "INSERT INTO tasks(`room-id`, `task-name`, `account-id`, `due-date`) VALUES($roomId, $taskName, 1, $dueDate)";
+
+
+if ($conn->query($sql) === TRUE) {
+    //echo "New record created successfully";
+    echo '<script> alert("New record created successfully") </script>';
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+// go back to calling screen after 1 second
+$url = $_SERVER['HTTP_REFERER']; // right back to the referrer page from where you came.
+echo '<meta http-equiv="refresh" content="1;URL=' . $url . '">';
+
+
+$conn->close();
+
+
+require 'functions.php';
--- views/signup.view.php
+++ views/signup.view.php
@@ -8,14 +8,19 @@
             <form action="signup" class="signup">
                 <label for="first-name">first name</label>
                 <input type="text" name="first-name">
+
                 <label for="last-name">last name</label>
                 <input type="text" name="last-name">
+
                 <label for="email">email address</label>
                 <input type="text" name="email">
+
                 <label for="password">password</label>
                 <input type="text" name="password">
+
                 <label for="confirm-password">confirm password</label>
                 <input type="text" name="confirm-password">
+                
                 <a href="index.php"><input type="submit" class="log-submit" value="SIGN UP"></a><!-- needs to link to user-creation -->
             </form>
         </div>
