48de96f448acde8cc3085f9488f636173fdd1aa3
abbeyGirvan <agirvan@northislandcollege.ca>
2023-11-06 17:38:04 -0800

    Create controllers/login.php for login validation

---
+++ controllers/login.php
@@ -0,0 +1,57 @@
+<?php
+
+require 'db-connection.php';
+require 'functions.php';
+
+// add single quote to start and end of data
+
+$email = "'" . $_POST["email"] . "'";
+$password = "'" . $_POST["password"] . "'";
+
+
+
+$sqlEmail = "SELECT * 
+FROM accounts
+WHERE `email` = $email;";
+
+$resultEmail = $conn->query($sqlEmail);
+
+// dd($resultEmail);
+
+
+
+if ($resultEmail->num_rows >= 1) {
+    echo '<script> alert("an account with this email already exists")</script>';
+    $url = $_SERVER['HTTP_REFERER']; // right back to the referrer page from where you came.
+    echo '<meta http-equiv="refresh" content="1;URL=' . $url . '">';
+} else {
+    if ($password !== $confirmPassword) {
+        echo '<script> alert("passwords do not match")</script>';
+        $url = $_SERVER['HTTP_REFERER']; // right back to the referrer page from where you came.
+        echo '<meta http-equiv="refresh" content="1;URL=' . $url . '">';
+    } else {
+    
+        // Insert record
+        $sql = "INSERT INTO accounts(`firstname`, `surname`, `email`, `password`) VALUES($firstName, $lastName, $email, $password)";
+    
+        //display message if new reccord created or display error if not
+        if ($conn->query($sql) === TRUE) {
+            //echo "New record created successfully";
+            echo '<script> alert("New record created successfully") </script>';
+            //redirect to index.php after creating an account
+            printf("<script>location.href='../index.php'</script>");
+        } else {
+            echo "Error: " . $sql . "<br>" . $conn->error;
+        }
+    }
+}
+
+
+
+
+
+
+$conn->close();
+
+
+
