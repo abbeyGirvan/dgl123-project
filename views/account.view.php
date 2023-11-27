4cd4aae2ca903b25b0bf1acd83c2e72579435a04
abbeyGirvan <agirvan@northislandcollege.ca>
2023-11-26 19:40:33 -0800

    Use session data to display account info

--- views/account.view.php
+++ views/account.view.php
@@ -1,4 +1,15 @@
-<?php require 'partials/head.php'; ?>
+<?php
+require 'partials/head.php';
+require 'controllers/db-connection.php';
+
+$sql = "SELECT firstname, surname, email 
+FROM accounts 
+WHERE id = $_SESSION[id];";
+
+$result = $conn->query($sql);
+
+$info = $result->fetch_assoc()
+?>
 
 <body>
     <main>
@@ -7,15 +18,17 @@
             <div>
                 <h2>YOU</h2>
                 <div class="current-user">
-                    <h3>Neil</h3> <!-- change to session-firstname -->
-                    <h3>Josten</h3> <!-- change to session-lastname -->
+                    <h3>first name:</h3>
+                    <p><?= $info["firstname"] ?></p>
+                    <h3>surname:</h3>
+                    <p><?= $info["surname"] ?></p> 
                 </div>
             </div>
             <div class="account-info">
                 <h2>ACCOUNT INFO</h2>
                 <h3>email:</h3>
-                <p>njosten@palmetofoxes.com</p> <!-- change to session-email -->
-                <h3>password</h3> 
+                <p><?= $info["email"] ?></p> <!-- change to session-email -->
+                <h3>password</h3>
                 <p>*********</p> <!-- change to session-password -->
             </div>
 
